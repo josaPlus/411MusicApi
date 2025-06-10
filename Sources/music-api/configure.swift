3e989aaab2a20f92c41f4caad3e39a3f4c3844d6
josaPlus <josafat.saldana@hotmial.comm>
2025-06-10 11:25:23 -0600

    modificacion

--- Sources/music-api/configure.swift
+++ Sources/music-api/configure.swift
@@ -8,14 +8,18 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+var tls = TLSConfiguration.makeClientConfiguration()
+tls.certificateVerification = .none
+
 app.databases.use(DatabaseConfigurationFactory.mysql(
     hostname: Environment.get("DATABASE_HOST")!,
     port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 25060,
     username: Environment.get("DATABASE_USERNAME")!,
     password: Environment.get("DATABASE_PASSWORD")!,
     database: Environment.get("DATABASE_NAME")!,
-    tlsConfiguration: .forClient(certificateVerification: .none)
+    tlsConfiguration: tls
 ), as: .mysql)
+
 
 
     app.migrations.add(CreateAlbum())
