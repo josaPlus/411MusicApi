c5558affa55c381c1624814cbbb8dabf81141600
josaPlus <josafat.saldana@hotmial.comm>
2025-06-10 12:04:55 -0600

    ejecute en el app plataform

--- Sources/music-api/configure.swift
+++ Sources/music-api/configure.swift
@@ -8,20 +8,15 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-var tls = TLSConfiguration.makeClientConfiguration()
-tls.certificateVerification = .none
-
 app.databases.use(DatabaseConfigurationFactory.mysql(
-    hostname: Environment.get("DATABASE_HOST")!,
-    port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 25060,
-    username: Environment.get("DATABASE_USERNAME")!,
-    password: Environment.get("DATABASE_PASSWORD")!,
-    database: Environment.get("DATABASE_NAME")!,
-    tlsConfiguration: tls
-), as: .mysql)
-
-
-
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 3310,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tlsConfiguration: .forClient(certificateVerification:.none)
+    ), as: .mysql)
+    
     app.migrations.add(CreateAlbum())
     app.migrations.add(CreateArtist())
     app.migrations.add(UpdateAlbum())
